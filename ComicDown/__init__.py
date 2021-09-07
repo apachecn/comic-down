56ba00ea25a058ebba9d848e3047dc6cdd5f88ba
wizardforcel <562826179@qq.com>
2021-09-07 17:47:25 +0800

    2021-09-07 17:47:25

---
+++ ComicDown/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3.7
+# -*- coding: utf-8 -*-
+
+"""ComicDown
+https://github.com/apachecn/comic-down"""
+
+from . import img
+from . import util
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "SATA"
+__version__ = "2021.9.7.0"
